11d121bc122cd1fa28ae89ce7bfaf391d26eae8d
thu lieu <thulieu2006ee@gmail.com>
2026-03-03 12:34:55 +0700

    hoàn thành

--- Models/Systems/BangChamCong.cs
+++ Models/Systems/BangChamCong.cs
@@ -4,6 +4,29 @@ namespace QuanLySieuThi.Models.Systems
 {
     public class BangChamCong
     {
-        Console.WriteLine("Le thi hong nha");
+        public string MaNV { get; set; }           
+        public DateTime NgayLamViec { get; set; }   
+        public string GioVao { get; set; }         
+        public string GioRa { get; set; }          
+        public string MaTheTV { get; set; }        
+
+        // Constructor không tham số
+        public BangChamCong() { }
+
+        // Constructor đầy đủ tham số
+        public BangChamCong(string maNV, DateTime ngayLam, string gioVao, string gioRa, string maThe)
+        {
+            MaNV = maNV;
+            NgayLamViec = ngayLam;
+            GioVao = gioVao;
+            GioRa = gioRa;
+            MaTheTV = maThe;
+        }
+
+        // Phương thức hiển thị thông tin (ví dụ)
+        public void HienThiThongTin()
+        {
+            Console.WriteLine($"NV: {MaNV} | Ngay: {NgayLamViec:dd/MM/yyyy} | Vao: {GioVao} | Ra: {GioRa}");
+        }
     }
 }
