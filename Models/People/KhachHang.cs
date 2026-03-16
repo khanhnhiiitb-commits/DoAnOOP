c858876eb462450248e4c4defb72061ac3db167a
Tang Khanh Nhi <KhanhNhiiitb@gmail.com>
2026-03-16 17:41:49 +0700

    i

--- Models/People/KhachHang.cs
+++ Models/People/KhachHang.cs
@@ -31,5 +31,17 @@ namespace QuanLySieuThi.Models.People
             get { return maTheTV; } 
             set { maTheTV = value; } 
         }
+
+        public KhachHang(){}
+
+        public KhachHang(string ma, string hoTen, DateTime ngaySinh, bool gioiTinh, string soDienThoai, string diaChi,
+                 string loaiKH, int diem, string maThe) 
+                 : base(ma, hoTen, ngaySinh, gioiTinh, soDienThoai, diaChi)
+            {
+            this.maKH = ma;
+            this.loaiKhachHang = loaiKH;
+            this.diemTichLuy = diem;
+            this.maTheTV = maThe;
+            }
     }
 }
